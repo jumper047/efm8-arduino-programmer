c474b274e334ce527d367e6c35014b52a93d4317
jumper047 <jumper047@gmail.com>
2020-11-24 23:09:43 +0300

    Move cxfreeze setup to separate file, add setuptools script

--- setup.py
+++ setup.py
@@ -1,99 +1,18 @@
-import os
-import re
-import sys
-import site
+from setuptools import setup
 
-from cx_Freeze import Executable, setup
-
-def get_qt_plugin_includes(plugin_list):
-    includes = []
-    for ppath in plugin_list:
-        includes.append(_get_include(ppath))
-        pass
-    return includes
-
-
-def _get_include(plugin_path):
-    foundPath = None
-
-    if sys.platform == "darwin":
-        packagesDirs = [c for c in sys.path if c.find("site-packages") != -1]
-    else:
-        # search site packages locations to see if we can find required .dll
-        packagesDirs = site.getsitepackages()
-        pass
-    for pdir in packagesDirs:
-        testPath = os.path.join(
-            pdir, os.path.join("PyQt5", "Qt", "plugins", plugin_path)
-        )
-        bname = os.path.basename(plugin_path)
-
-        # print("Checking for {} at {}".format(bname, testPath))
-        if os.path.exists(testPath):
-            foundPath = testPath
-            # print("DLL Found")
-            break
-        pass
-    if foundPath is None:
-        print(f"Error, could not find: {plugin_path}")
-        sys.exit(1)
-
-    return (foundPath, plugin_path)
-
-build_dir = "build"
-include_files = [("bin", "bin")]
-
-required_plugins = []
-if sys.platform == "win32":
-    # force the inclusion of certain plugins that cx-freeze cannot find on its own
-    required_plugins = [
-        "styles/qwindowsvistastyle.dll"
-    ]
-
-# # fix for building with wine
-# try:
-#     winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, "Software\Wine")
-# except FileNotFoundError:
-#     pass
-# else:
-#     for py_dir in site.getsitepackages():
-#         base_dir, _, files = next(os.walk(py_dir))
-#         if 'python3.dll' not in files:
-#             continue
-#         python3_dll = os.path.join(py_dir, 'python3.dll')
-#         vcruntime140_dll = os.path.join(py_dir, 'vcruntime140.dll')
-#         for f in files:
-#             if re.match('python3[0-9]+\.dll', f):
-#                 python3x_dll = os.path.join(py_dir, f)
-#                 python3x_name = f
-#                 break
-#     include_files += [(python3_dll, os.path.join('lib','PyQt5', 'python3.dll')),
-#                       (vcruntime140_dll, 'vcruntime140.dll'),
-#                       (python3x_dll, python3x_name)]
-
-if len(required_plugins) > 0:
-    include_files += get_qt_plugin_includes(plugin_list=required_plugins)
-
-options = {
-    'build_exe': {
-        'build_exe': build_dir,
-        'include_files': include_files, 
-        'include_msvcr': True,
-        'zip_include_packages': ["PyQt5"],
-        'packages': ["PyQt5.sip"]
-    }
-}
-
-base = None
-# if sys.platform == "win32":
-#     base = "Win32GUI"
-
-executables = [
-    Executable('gui_flasher.py', base=base)
-]
-
-setup(name='efm8-programmer',
-      version="1.0.0",
-      description='EFM8 Programmer',
-      options=options,
-      executables=executables)
+setup(name='efm8_arduino_programmer',
+      version='1.0',
+      description='Program efm8 micro controller via arduino board',
+      url='http://github.com/jumper047/efm8_arduino_programmer',
+      packages=['efm8_arduino_programmer'],
+      zip_safe=False,
+      install_requires=['pyserial'],
+      extras_require={
+          "gui": "PyQt5"}, 
+      entry_points = {
+          "console_scripts": [
+              "efm8_flash = efm8_arduino_programmer.scripts:flash",
+              "efm8_read = efm8_arduino_programmer.scripts:read",
+              "efm8_arduino_flash = efm8_arduino_programmer.scripts:flash_arduino"],
+          "gui_scripts": ["efm8_programmer = efm8_arduino_programmer.efm8_arduino_programmer:main [gui]"]}
+      )
---
+++ setup_cxFreeze.py
@@ -0,0 +1,78 @@
+import os
+import re
+import sys
+import site
+
+from cx_Freeze import Executable, setup
+
+def get_qt_plugin_includes(plugin_list):
+    includes = []
+    for ppath in plugin_list:
+        includes.append(_get_include(ppath))
+        pass
+    return includes
+
+
+def _get_include(plugin_path):
+    foundPath = None
+
+    if sys.platform == "darwin":
+        packagesDirs = [c for c in sys.path if c.find("site-packages") != -1]
+    else:
+        # search site packages locations to see if we can find required .dll
+        packagesDirs = site.getsitepackages()
+        pass
+    for pdir in packagesDirs:
+        testPath = os.path.join(
+            pdir, os.path.join("PyQt5", "Qt", "plugins", plugin_path)
+        )
+        bname = os.path.basename(plugin_path)
+
+        # print("Checking for {} at {}".format(bname, testPath))
+        if os.path.exists(testPath):
+            foundPath = testPath
+            # print("DLL Found")
+            break
+        pass
+    if foundPath is None:
+        print(f"Error, could not find: {plugin_path}")
+        sys.exit(1)
+
+    return (foundPath, plugin_path)
+
+build_dir = "build"
+include_files = [("bin", "bin")]
+
+required_plugins = []
+if sys.platform == "win32":
+    # force the inclusion of certain plugins that cx-freeze cannot find on its own
+    required_plugins = [
+        "styles/qwindowsvistastyle.dll"
+    ]
+
+if len(required_plugins) > 0:
+    include_files += get_qt_plugin_includes(plugin_list=required_plugins)
+
+options = {
+    'build_exe': {
+        'build_exe': build_dir,
+        'include_files': include_files, 
+        'include_msvcr': True,
+        'zip_include_packages': ["PyQt5"],
+        'packages': ["PyQt5.sip"]
+    }
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+    Executable('gui_flasher.py', base=base)
+]
+
+setup(name='efm8-programmer',
+      version="1.0.0",
+      description='EFM8 Programmer',
+      options=options,
+      executables=executables)
